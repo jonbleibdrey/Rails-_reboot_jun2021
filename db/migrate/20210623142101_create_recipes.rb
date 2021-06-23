6b658d6b66421ad2b5d08bee4e24d79e5c8253e3
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-06-23 11:09:40 -0400

    added database

--- db/migrate/20210623142101_create_recipes.rb
+++ db/migrate/20210623142101_create_recipes.rb
@@ -2,12 +2,13 @@ class CreateRecipes < ActiveRecord::Migration[6.1]
   def change
     create_table :recipes do |t|
       t.string :title
+      t.string :image
+      t.string :recipe_ingr
       t.string :description
       t.string :genre
       t.boolean :like
-      t.binary :image
       t.integer :user_id
-      
+
       t.timestamps
     end
   end
